be1958c575ef0841a5712da8c594be311abc9c7d
Nelson Perez <elnelsonperez@gmail.com>
2025-04-01 13:49:44 -0400

    Use node 22

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(), TanStackRouterVite()],
+  plugins: [react(), tailwindcss()],
 })
